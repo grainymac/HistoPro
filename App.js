10c4381f587865a1dccaac481ceff591817b5aa1
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-04-20 06:30:35 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const { height } = Dimensions.get('window')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={[styles.verticalBar, { height }]}>
+        <Text style={styles.timeText}>{new Date().toLocaleTimeString()}</Text>
+        <Text style={styles.locationText}>Current Location:</Text>
+      </View>
+      <Text>HistoPro</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3d7fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  verticalBar: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 45,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  timeText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    width: 100,
+    margin: 0,
+    transform: [{ rotate: '90deg' }],
+  },
+  locationText: {
+    fontSize: 10,
+    width: 100,
+    textAlign: 'center',
+    transform: [{ rotate: '90deg' }],
   },
 });
